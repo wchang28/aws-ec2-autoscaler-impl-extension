fd2f4093547a55f16b9a8b78dbc619c79cd816de
wchang28 <wchang28@hotmail.com>
2017-03-18 18:43:55 -0400

    gtbrbtw

---
+++ ts/ui/main.tsx
@@ -0,0 +1,4 @@
+import {GridClient} from 'grid-client-browser';
+import {IImplementation, getImplementation} from '../implApi';
+
+let impl = getImplementation(GridClient.getSession().AutoScalerImplementationApiCore);
