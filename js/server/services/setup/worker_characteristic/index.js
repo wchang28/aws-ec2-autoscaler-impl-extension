e37b5b3b4d5754e0f61231d7fad3c3f7a62b51a7
wchang28 <wchang28@hotmail.com>
2017-03-19 00:31:37 -0400

    ghgdnhjd

--- js/server/services/setup/worker_characteristic/index.js
+++ js/server/services/setup/worker_characteristic/index.js
@@ -1,5 +1,45 @@
 "use strict";
 // route /services/setup/worker_characteristic
 var express = require("express");
+var requestData_1 = require("../../../requestData");
 var router = express.Router();
 exports.Router = router;
+/*
+    /services/setup/worker_characteristic
+    /services/setup/worker_characteristic/get_key_name
+    /services/setup/worker_characteristic/set_key_name
+    /services/setup/worker_characteristic/get_instance_type
+    /services/setup/worker_characteristic/set_instance_type
+    /services/setup/worker_characteristic/get_image_id
+    /services/setup/worker_characteristic/set_image_id
+    /services/setup/worker_characteristic/get_security_group_id
+    /services/setup/worker_characteristic/set_security_group_id
+    /services/setup/worker_characteristic/get_subnet_id
+    /services/setup/worker_characteristic/set_subnet_id
+ */
+router.get('/', requestData_1.getReqestHandler(function (impl, req) { return Promise.resolve(impl.WorkerCharacteristic); }));
+router.get('/get_key_name', requestData_1.getReqestHandler(function (impl, req) { return Promise.resolve(impl.WorkerCharacteristic.KeyName); }));
+router.post('/set_key_name', requestData_1.getReqestHandler(function (impl, req) {
+    impl.WorkerCharacteristic.KeyName = req.body;
+    return Promise.resolve({});
+}));
+router.get('/get_instance_type', requestData_1.getReqestHandler(function (impl, req) { return Promise.resolve(impl.WorkerCharacteristic.InstanceType); }));
+router.post('/set_instance_type', requestData_1.getReqestHandler(function (impl, req) {
+    impl.WorkerCharacteristic.InstanceType = req.body;
+    return Promise.resolve({});
+}));
+router.get('/get_image_id', requestData_1.getReqestHandler(function (impl, req) { return Promise.resolve(impl.WorkerCharacteristic.ImageId); }));
+router.post('/set_image_id', requestData_1.getReqestHandler(function (impl, req) {
+    impl.WorkerCharacteristic.ImageId = req.body;
+    return Promise.resolve({});
+}));
+router.get('/get_security_group_id', requestData_1.getReqestHandler(function (impl, req) { return Promise.resolve(impl.WorkerCharacteristic.SecurityGroupId); }));
+router.post('/set_security_group_id', requestData_1.getReqestHandler(function (impl, req) {
+    impl.WorkerCharacteristic.SecurityGroupId = req.body;
+    return Promise.resolve({});
+}));
+router.get('/get_subnet_id', requestData_1.getReqestHandler(function (impl, req) { return Promise.resolve(impl.WorkerCharacteristic.SubnetId); }));
+router.post('/set_subnet_id', requestData_1.getReqestHandler(function (impl, req) {
+    impl.WorkerCharacteristic.SubnetId = req.body;
+    return Promise.resolve({});
+}));
--- ts/server/services/setup/worker_characteristic/index.ts
+++ ts/server/services/setup/worker_characteristic/index.ts
@@ -6,3 +6,45 @@ import {Implementation as EC2Implementation} from "aws-ec2-autoscaler-impl";
 
 let router = express.Router();
 export {router as Router};
+
+/*
+    /services/setup/worker_characteristic
+    /services/setup/worker_characteristic/get_key_name
+    /services/setup/worker_characteristic/set_key_name
+    /services/setup/worker_characteristic/get_instance_type
+    /services/setup/worker_characteristic/set_instance_type
+    /services/setup/worker_characteristic/get_image_id
+    /services/setup/worker_characteristic/set_image_id
+    /services/setup/worker_characteristic/get_security_group_id
+    /services/setup/worker_characteristic/set_security_group_id
+    /services/setup/worker_characteristic/get_subnet_id
+    /services/setup/worker_characteristic/set_subnet_id
+ */
+
+router.get('/', getReqestHandler((impl: EC2Implementation, req: express.Request) => Promise.resolve<any>(impl.WorkerCharacteristic)));
+router.get('/get_key_name', getReqestHandler((impl: EC2Implementation, req: express.Request) => Promise.resolve<any>(impl.WorkerCharacteristic.KeyName)));
+router.post('/set_key_name', getReqestHandler((impl: EC2Implementation, req: express.Request) => {
+    impl.WorkerCharacteristic.KeyName = req.body;
+    return Promise.resolve<any>({});
+}));
+router.get('/get_instance_type', getReqestHandler((impl: EC2Implementation, req: express.Request) => Promise.resolve<any>(impl.WorkerCharacteristic.InstanceType)));
+router.post('/set_instance_type', getReqestHandler((impl: EC2Implementation, req: express.Request) => {
+    impl.WorkerCharacteristic.InstanceType = req.body;
+    return Promise.resolve<any>({});
+}));
+router.get('/get_image_id', getReqestHandler((impl: EC2Implementation, req: express.Request) => Promise.resolve<any>(impl.WorkerCharacteristic.ImageId)));
+router.post('/set_image_id', getReqestHandler((impl: EC2Implementation, req: express.Request) => {
+    impl.WorkerCharacteristic.ImageId = req.body;
+    return Promise.resolve<any>({});
+}));
+router.get('/get_security_group_id', getReqestHandler((impl: EC2Implementation, req: express.Request) => Promise.resolve<any>(impl.WorkerCharacteristic.SecurityGroupId)));
+router.post('/set_security_group_id', getReqestHandler((impl: EC2Implementation, req: express.Request) => {
+    impl.WorkerCharacteristic.SecurityGroupId = req.body;
+    return Promise.resolve<any>({});
+}));
+
+router.get('/get_subnet_id', getReqestHandler((impl: EC2Implementation, req: express.Request) => Promise.resolve<any>(impl.WorkerCharacteristic.SubnetId)));
+router.post('/set_subnet_id', getReqestHandler((impl: EC2Implementation, req: express.Request) => {
+    impl.WorkerCharacteristic.SubnetId = req.body;
+    return Promise.resolve<any>({});
+}));
