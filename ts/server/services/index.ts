2be5165dd9f2bb99f6958ded67926999531586d5
wchang28 <wchang28@hotmail.com>
2017-03-19 00:15:42 -0400

    hjfhm

--- ts/server/services/index.ts
+++ ts/server/services/index.ts
@@ -16,7 +16,6 @@ let router = express.Router();
     /services/setup
 */
 
-
 router.post('/translate_to_worker_keys', getReqestHandler((impl: EC2Implementation, req: express.Request) => impl.TranslateToWorkerKeys(req.body)));
 router.post('/estimate_workers_launch_request', getReqestHandler((impl: EC2Implementation, req: express.Request) => impl.EstimateWorkersLaunchRequest(req.body)));
 router.post('/launch_instances', getReqestHandler((impl: EC2Implementation, req: express.Request) => impl.LaunchInstances(req.body)));
