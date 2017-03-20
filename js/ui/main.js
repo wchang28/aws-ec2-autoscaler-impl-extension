0838d7e5d607aa10eb1ee7c801da035f27cca1b1
wchang28 <wchang28@hotmail.com>
2017-03-20 00:12:00 -0400

    add html

--- js/ui/main.js
+++ js/ui/main.js
@@ -1,8 +1,68 @@
 "use strict";
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var React = require("react");
+var ReactDOM = require("react-dom");
 var grid_client_browser_1 = require("grid-client-browser");
 var implApi_1 = require("../implApi");
 var implApiCore = grid_client_browser_1.GridClient.getSession().AutoScalerImplementationApiCore;
-implApiCore.$M().subscribe("/", function (msg) {
-});
 var impl = implApi_1.getImplementation(implApiCore);
-//impl.Setup.toJSON() 
+var ImplApp = (function (_super) {
+    __extends(ImplApp, _super);
+    function ImplApp(props) {
+        var _this = _super.call(this, props) || this;
+        _this.msgClient = null;
+        _this.state = { conn_id: null, sub_id: null, setup: null };
+        return _this;
+    }
+    Object.defineProperty(ImplApp.prototype, "Implementation", {
+        get: function () { return this.props.implementation; },
+        enumerable: true,
+        configurable: true
+    });
+    ImplApp.prototype.componentDidMount = function () {
+        var _this = this;
+        console.log('componentDidMount()');
+        this.Implementation.Setup.toJSON()
+            .then(function (setup) {
+            _this.setState({ setup: setup });
+        }).catch(function (err) {
+            console.error('!!! error:' + JSON.stringify(err));
+        });
+        this.msgClient = this.props.apiCore.$M();
+        this.msgClient.on('connect', function (conn_id) {
+            console.log('connected to the dispatcher: conn_id=' + conn_id);
+            _this.setState({ conn_id: conn_id });
+            _this.msgClient.subscribe("/", function (msg) {
+                _this.Implementation.Setup.toJSON()
+                    .then(function (setup) {
+                    _this.setState({ setup: setup });
+                }).catch(function (err) {
+                    console.error('!!! error:' + JSON.stringify(err));
+                });
+            }, {})
+                .then(function (sub_id) {
+                _this.setState({ sub_id: sub_id });
+            }).catch(function (err) {
+                console.error('!!! subscription error:' + JSON.stringify(err));
+                _this.setState({ sub_id: null });
+            });
+        }).on('error', function (err) {
+            console.error('!!! connection error:' + JSON.stringify(err));
+            _this.setState({ conn_id: null });
+        });
+    };
+    ImplApp.prototype.componentWillUnmount = function () {
+        console.log('componentWillUnmount()');
+        this.msgClient.disconnect();
+        this.msgClient = null;
+    };
+    ImplApp.prototype.render = function () {
+        return React.createElement("div", null, this.state.setup ? JSON.stringify(this.state.setup, null, 2) : "???");
+    };
+    return ImplApp;
+}(React.Component));
+ReactDOM.render(React.createElement(ImplApp, { implementation: impl, apiCore: implApiCore }), document.getElementById('main'));
--- ts/ui/main.tsx
+++ ts/ui/main.tsx
@@ -1,9 +1,71 @@
-import {GridClient, GridMessage} from 'grid-client-browser';
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import {GridClient, GridMessage, ApiCore, IMessageClient} from 'grid-client-browser';
 import {IImplementation, getImplementation} from '../implApi';
+import {ImplementationJSON} from "aws-ec2-autoscaler-impl";
 
 let implApiCore = GridClient.getSession().AutoScalerImplementationApiCore;
-implApiCore.$M().subscribe("/", (msg: GridMessage) => {
-
-});
 let impl = getImplementation(implApiCore);
-//impl.Setup.toJSON()
+
+interface ImplAppProps {
+    implementation: IImplementation;
+    apiCore: ApiCore<GridMessage>;
+}
+
+interface ImplAppState {
+    conn_id?: string;
+    sub_id?: string;
+    setup?: ImplementationJSON
+}
+
+class ImplApp extends React.Component<ImplAppProps, ImplAppState> {
+    private msgClient: IMessageClient<GridMessage>;
+    constructor(props:ImplAppProps) {
+        super(props);
+        this.msgClient = null;
+        this.state = {conn_id: null, sub_id: null, setup: null};
+    }
+    get Implementation(): IImplementation {return this.props.implementation;}
+    componentDidMount() {
+        console.log('componentDidMount()')
+        this.Implementation.Setup.toJSON()
+        .then((setup: ImplementationJSON) => {
+            this.setState({setup});
+        }).catch((err: any) => {
+            console.error('!!! error:' + JSON.stringify(err));
+        });
+        this.msgClient = this.props.apiCore.$M();
+        this.msgClient.on('connect', (conn_id:string) => {
+            console.log('connected to the dispatcher: conn_id=' + conn_id);
+            this.setState({conn_id});
+            this.msgClient.subscribe("/", (msg: GridMessage) => {
+                this.Implementation.Setup.toJSON()
+                .then((setup: ImplementationJSON) => {
+                    this.setState({setup});
+                }).catch((err: any) => {
+                    console.error('!!! error:' + JSON.stringify(err));
+                });
+            }, {})
+            .then((sub_id: string) => {
+                this.setState({sub_id});
+            }).catch((err: any) => {
+                console.error('!!! subscription error:' + JSON.stringify(err));
+                this.setState({sub_id: null});
+            });
+        }).on('error', (err: any) => {
+            console.error('!!! connection error:' + JSON.stringify(err));
+            this.setState({conn_id: null});
+        });
+    }
+    componentWillUnmount() {
+        console.log('componentWillUnmount()');
+        this.msgClient.disconnect();
+        this.msgClient = null;
+    }
+    render() {
+        return <div>{this.state.setup ? JSON.stringify(this.state.setup, null, 2) : "???"}</div>;
+    }
+}
+
+ReactDOM.render(<ImplApp implementation={impl} apiCore={implApiCore}/>, document.getElementById('main'));
